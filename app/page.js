c397edc744977c6709b14255bc5e60c0dfdf1b7f
Eugene Kvasnyi <evghenii.cvasnii@boosta.co>
2023-05-14 13:22:57 +0300

    Update markup

--- app/page.js
+++ app/page.js
@@ -4,15 +4,23 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        ReCropped.co
-      </div>
+      <div className={styles.description}>ReCropped.co</div>
 
       <div className={styles.center}>
-        <p>Dynamic Dummy Image Generator service</p>
+        <p>Placeholder Image Generator service</p>
       </div>
-
       <div className={styles.grid}>
+      <a
+          href="https://api.recropped.co/image?width=500&height=200"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Get started <span>-&gt;</span>
+          </h2>
+          <p>Click here to start using our service API</p>
+        </a>
         <a
           href="https://docs.recropped.co"
           className={styles.card}
